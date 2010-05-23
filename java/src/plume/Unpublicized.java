a864773156b347ee4ba91a7d35df02289af0291f
Michael Ernst <mernst@cs.washington.edu>
2010-05-23 13:19:00 -0700

    Fix Javadoc errors

---
+++ java/src/plume/Unpublicized.java
@@ -0,0 +1,24 @@
+// The three files
+//   Option.java
+//   Options.java
+//   Unpublicized.java
+// together comprise the implementation of command-line processing.
+
+package plume;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used in conjunction with the @Option annotation to signal that
+ * the option should not be included in the usage message.
+ * @see plume.Option
+ * @see plume.Options
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Unpublicized {
+
+}
