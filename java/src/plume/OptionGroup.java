b6ed56e45c9ba267d46b1cf1dcf91e918a84810b
Michael Ernst <mernst@cs.washington.edu>
2010-06-08 20:41:32 -0700

    Small improvements

--- java/src/plume/OptionGroup.java
+++ java/src/plume/OptionGroup.java
@@ -12,9 +12,12 @@ import java.lang.annotation.*;
 /**
  * Used in conjunction with the <code>@Option</code> annotation to indicate that
  * the following <code>@Option</code>-annotated fields (including the one the
- * <code>@OptionGroup</code> annotation is being applied to) belong to the same
- * option group.
+ * <code>@OptionGroup</code> annotation is applied to) belong to the same
+ * option group.  Option groups are documented in {@link Options}.
+ *
  * @see plume.Options
+ * @see plume.Option
+ * @see plume.Unpublicized
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
