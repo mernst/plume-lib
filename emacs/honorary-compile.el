7dc99817882f8c9be12f40f1a7887e01969a35e4
David Lazar <lazar6@illinois.edu>
2010-06-10 12:13:16 -0500

    Use empty array as default value for aliases and remove redundant has_alias field.

---
+++ emacs/honorary-compile.el
@@ -0,0 +1,54 @@
+;;; honorary-compile.el --- treat some shell buffers as compilation buffers
+
+(defvar honorary-compilation-minor-mode nil
+  "Non-nil if the current buffer should be considered a compilation buffer,
+but without having the keymap installed.")
+(make-variable-buffer-local 'honorary-compilation-minor-mode)
+
+;; This should probably be able to toggle.
+;; Maybe don't bother permitting buffer specification.
+(defun honorary-compilation-buffer (&optional buffer)
+  "Make the specified or current buffer be considered a compilation buffer."
+  (interactive)
+  ;; Equally efficient to do save-excursion and set-buffer unconditionally?
+  (if (or (not buffer) (eq buffer (current-buffer)))
+      (setq honorary-compilation-minor-mode t)
+    (save-excursion
+      (set-buffer buffer)
+      (setq honorary-compilation-minor-mode t))))
+
+;; ordinarily, only checks compilation-mode and compilation-minor-mode
+(defadvice compilation-buffer-p (after honorary-compilation-buffer activate)
+  (if (not ad-return-value)
+      (save-excursion
+	(set-buffer (ad-get-arg 0))	; 0 = first arg, which is buffer name
+	(setq ad-return-value honorary-compilation-minor-mode))))
+
+;; In particular, if Vortex is running in a shell buffer, put point in that
+;; buffer and run next-error.
+(defadvice compilation-find-buffer (around use-current-buffer activate)
+  "If in shell mode, no compilation started, and errors found, use current buffer."
+  (condition-case err
+      ad-do-it
+    (error
+     (if (and (eq (car err) 'error)
+	      (equal (car (cdr err)) "No compilation started!")
+	      (eq major-mode 'shell-mode)
+	      ;; (save-excursion
+	      ;;   (goto-char (point-min))
+	      ;;   (re-search-forward error-file-line-re nil t))
+	      )
+	 (progn
+	   ;; Prevent compilation-setup from modifying mode-line-process
+	   (let ((old-mode-line-process mode-line-process))
+	     ;; Don't do this, because it installs compilation-minor-mode-map
+	     ;; (compilation-minor-mode 1)	; arg 1 = turn on
+	     (compilation-setup)
+	     (setq mode-line-process old-mode-line-process))
+	   (setq honorary-compilation-minor-mode t)
+	   (setq ad-return-value (current-buffer)))
+       (signal (car err) (cdr err))))))
+
+(provide 'honorary-compile)
+
+;;; honorary-compile.el ends here
---
+++ emacs/mail-utils-mde.el
@@ -0,0 +1,85 @@
+;;; Header-manipulation functions useful both in sending and receiving mail.
+
+(eval-when-compile '(require 'sendmail))
+
+;;;
+;;; Headers
+;;;
+
+(defun mail-field-extent (field)
+  "Return nil or a cons of (start . end) for the field.
+Start is at the beginning of a line and end is at the end of a line."
+  (save-excursion
+    (and (mail-position-on-field field t)
+	 (let ((end (point)))
+	   (re-search-backward "^[^ \t]" nil 'move)
+	   (cons (point) end)))))
+
+(defun mail-delete-field (field)
+  (let ((region (mail-field-extent field)))
+    (if region
+	(delete-region (car region) (1+ (cdr region))))))
+
+(defun mail-make-field-invisible (field)
+  (let ((region (mail-field-extent field)))
+    (if region
+	(put-text-property (car region) (1+ (cdr region)) 'invisible t))))
+
+
+;; Could use mail-position-on-field in the following two functions, but it
+;; does extra work when we know the field exists and don't need to find end
+;; of mail headers, etc.  Also, mail-position-on-field requires that
+;; mail-header-separator be found, so it isn't useful on messages being
+;; read, only those being composed.
+(defun mail-remove-field (field)
+  (save-match-data
+    (if (let ((case-fold-search t)
+	      (header-end (rmail-header-end)))
+	  (goto-char (point-min))
+	  (re-search-forward (concat "^" (regexp-quote field) ":") header-end t))
+	(progn
+	  (beginning-of-line)
+	  (delete-region (point)
+			 (progn (re-search-forward "\n[^ \t]")
+				(forward-char -1)
+				(point)))))))
+
+(defun mail-set-field (field text &optional add)
+  "If optional argument ADD is non-nil, the field is added if it did not
+previously exist.  Otherwise, if it did not exist, no action is taken."
+  (save-match-data
+    (if (or (let ((case-fold-search t)
+		  (header-end (rmail-header-end)))
+	      (goto-char (point-min))
+	      (re-search-forward (concat "^" (regexp-quote field) ":") header-end t))
+	    (and add
+		 (progn
+		   (goto-char (point-min))
+		   (insert field ": \n")
+		   (backward-char 2)
+		   t)))
+	;; point is not just after the colon and before a space or tab
+	(progn
+	  (forward-char 1)
+	  (delete-region (point)
+			 (progn (re-search-forward "\n[^ \t]")
+				(forward-char -2)
+				(point)))
+	  (while (string-match "\n[^ \t]" text)
+	    (setq text (concat (substring text 0 (1+ (match-beginning 0)))
+			       "\t"
+			       (substring text (1- (match-end 0))))))
+	  (insert text)))))
+
+;; `mail-header-end' doesn't work in an RMAIL buffer, because it widens and
+;; then goes to the beginning of the buffer.  It does work well in a *mail*
+;; buffer, however.  But this works in that case, too.
+(defun rmail-header-end ()
+  (save-excursion
+    (goto-char (point-min))
+    (rfc822-goto-eoh)
+    (point)))
+
+;;;
+
+(provide 'mail-utils-mde)
