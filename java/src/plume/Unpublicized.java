ec064322eb7398713fbbef7828f400c315fa144b
Werner Dietl <wdietl@gmail.com>
2010-07-30 15:00:43 -0700

    Add commons-lang as new dependency.

---
+++ java/src/plume/Unpublicized.java
@@ -0,0 +1,29 @@
+// The five files
+//   Option.java
+//   OptionGroup.java
+//   Options.java
+//   Unpublicized.java
+//   OptionsDoclet.java
+// together comprise the implementation of command-line processing.
+
+package plume;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used in conjunction with the @Option annotation to signal that
+ * the option should not be included in the usage message, unless an optional
+ * include_unpublicized argument is supplied and is true.
+ * @see plume.Option
+ * @see plume.Options
+ * @see plume.OptionGroup
+ * @see plume.OptionsDoclet
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Unpublicized {
+
+}
