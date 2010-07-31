f34753b44ddcf60deca23ace679d14d1c57b7e91
Michael Ernst <mernst@cs.washington.edu>
2010-07-31 15:40:53 -0700

    Improve documentation

--- bin/striplines.pl
+++ bin/striplines.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w -p -i.bak
 # striplines.pl
 # Strips '#line' directives out of a file.  The file is modified in place,
-# but a backup is made to ".bak".
+# but a backup is made to "<filename>.bak".
 
 if (/^\#line/) {
   $_ = undef;
