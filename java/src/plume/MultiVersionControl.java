3de04739f3c2ebc83863f3b43dcbced919370541
Michael Ernst <mernst@cs.washington.edu>
2011-01-22 13:20:15 -0800

    Handle replacers that match multiple times in overlapping locations.

--- java/src/plume/MultiVersionControl.java
+++ java/src/plume/MultiVersionControl.java
@@ -962,7 +962,7 @@ public class MultiVersionControl {
         // Hack, should be replaced when googlecode certificate problems are fixed.
         replacers.add(new Replacer("(^|\\n)warning: .* certificate not verified \\(check web.cacerts config setting\\)\\n", "$1"));
         // Does this mask too many errors?
-        replacers.add(new Replacer("(^|\\n)(abort: repository default(-push)? not found!: .*\\n)", ""));
+        replacers.add(new Replacer("(^|\\n)(abort: repository default(-push)? not found!: .*\\n)", "$1"));
         break;
       case SVN:
         replacers.add(new Replacer("(svn: Network connection closed unexpectedly)", "$1 for " + dir));
@@ -1254,7 +1254,12 @@ public class MultiVersionControl {
         String output = UtilMDE.streamString(p.getInputStream());
         if (debug_replacers) { System.out.println("preoutput=<<<" + output + ">>>"); }
         for (Replacer r : replacers) {
-          output = r.replaceAll(output);
+          String old_output = "";
+          // Loop because the regexp may match in overlapping locations
+          while (! output.equals(old_output)) {
+            old_output = output;
+            output = r.replaceAll(output);
+          }
           if (debug_replacers) { System.out.println("midoutput[" + r.regexp + "]=<<<" + output + ">>>"); }
         }
         if (debug_replacers) {
