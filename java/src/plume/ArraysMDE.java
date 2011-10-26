8a1157f3c110d3a57d050fd12a0187339c304a0f
Michael Ernst <mernst@cs.washington.edu>
2011-10-26 10:14:41 -0700

    Experimentally comment out a version of concat

--- java/src/plume/ArraysMDE.java
+++ java/src/plume/ArraysMDE.java
@@ -1258,31 +1258,32 @@ public final class ArraysMDE {
     }
   }
 
-  /**
-   * Return an array that contains all the elements of both argument
-   * arrays, in order.
-   * Returns a new array unless one argument is null, in which case
-   * it returns the other array.
-   **/
-  public static String[] concat(String /*@Nullable*/ [] a, String /*@Nullable*/ [] b) {
-    if (a == null) {
-      if (b == null) {
-        return new String[0];
-      } else {
-        return b;
-      }
-    } else {
-      if (b == null) {
-        return a;
-      } else {
-        String[] result = new String[a.length + b.length];
-
-        System.arraycopy(a, 0, result, 0, a.length);
-        System.arraycopy(b, 0, result, a.length, b.length);
-        return result;
-      }
-    }
-  }
+// Subsumed by generic version?
+//   /**
+//    * Return an array that contains all the elements of both argument
+//    * arrays, in order.
+//    * Returns a new array unless one argument is null, in which case
+//    * it returns the other array.
+//    **/
+//   public static /*@PolyInterned*/ String[] concat(/*@PolyInterned*/ String /*@Nullable*/ [] a, /*@PolyInterned*/ String /*@Nullable*/ [] b) {
+//     if (a == null) {
+//       if (b == null) {
+//         return new String[0];
+//       } else {
+//         return b;
+//       }
+//     } else {
+//       if (b == null) {
+//         return a;
+//       } else {
+//         String[] result = new String[a.length + b.length];
+// 
+//         System.arraycopy(a, 0, result, 0, a.length);
+//         System.arraycopy(b, 0, result, a.length, b.length);
+//         return result;
+//       }
+//     }
+//   }
 
 
   /**
