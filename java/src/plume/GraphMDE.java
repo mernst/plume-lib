0fe01a2462cf23dfb9532accfa338a58a353f974
Michael Ernst <mernst@cs.washington.edu>
2011-12-12 20:45:32 -0800

    Clarify @SuppressWarnings(nullness) text

--- java/src/plume/GraphMDE.java
+++ java/src/plume/GraphMDE.java
@@ -83,7 +83,7 @@ public class GraphMDE {
             new_doms.retainAll(dom_of_pred);
           }
         }
-        assert new_doms != null : "@SuppressWarnings(nullness): not sure why; investigate later, and what to do if it is null";
+        assert new_doms != null : "@SuppressWarnings(nullness): not sure why it's guaranteed to be non-null; investigate later, and decide what to do if it can be null";
         new_doms.add(node);
         assert dom.containsKey(node);
         if (! dom.get(node).equals(new_doms)) {
