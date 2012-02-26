e03585e7fbe0cd19b9d523a0c7e559257cf5cdf5
Michael Ernst <mernst@cs.washington.edu>
2012-02-26 15:43:19 -0800

    Tweak quoting rules for Google searches

---
+++ java/src/plume/RegexUtil.java
@@ -0,0 +1,89 @@
+package plume;
+
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+// This class should be kept in sync with checkers.regex.RegexUtil .
+
+/**
+ * Utility methods for regular expressions.
+ * <p>
+ *
+ * For an example of intended use, see section <a
+ * href="http://types.cs.washington.edu/checker-framework/current/checkers-manual.html#regexutil-methods">Testing
+ * whether a string is a regular expression</a> in the Checker Framework
+ * manual.
+ * <p>
+ */
+public class RegexUtil {
+
+  private RegexUtil() {
+    throw new AssertionError("Class RegexUtil shouldn't be instantiated");
+  }
+
+  // These methods should be kept in sync with those in plume.UtilMDE .
+
+  /** 
+   * Returns true if the argument is a syntactically valid regular
+   * expression. 
+   */
+  @SuppressWarnings("regex")    // RegexUtil
+  /*@Pure*/
+  public static boolean isRegex(String s) {
+    try {
+      Pattern.compile(s);
+    } catch (PatternSyntaxException e) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Returns null if the argument is a syntactically valid regular
+   * expression. Otherwise returns a string describing why the string is
+   * not a regex.
+   */
+  @SuppressWarnings("regex")    // RegexUtil
+  /*@Pure*/
+  public static /*@Nullable*/ String regexError(String s) {
+    try {
+      Pattern.compile(s);
+    } catch (PatternSyntaxException e) {
+      return e.getMessage();
+    }
+    return null;
+  }
+  
+  /**
+   * Returns null if the argument is a syntactically valid regular
+   * expression. Otherwise returns a PatternSyntaxException describing
+   * why the string is not a regex.
+   */
+  @SuppressWarnings("regex")    // RegexUtil
+  /*@Pure*/
+  public static /*@Nullable*/ PatternSyntaxException regexException(String s) {
+    try {
+      Pattern.compile(s);
+    } catch (PatternSyntaxException pse) {
+      return pse;
+    }
+    return null;
+  }
+
+  /**
+   * Returns the argument if it is a regex, otherwise throws an error.
+   * The purpose of this method is to suppress Regex Checker warnings.
+   * Once the the Regex Checker supports flow-sensitivity, it should be
+   * very rarely needed.
+   */
+  @SuppressWarnings("regex")    // RegexUtil
+  /*@Pure*/
+  public static /*@Regex*/ String asRegex(String s) {
+    try {
+      Pattern.compile(s);
+      return s;
+    } catch (PatternSyntaxException e) {
+      throw new Error(e);
+    }
+  }
+}
