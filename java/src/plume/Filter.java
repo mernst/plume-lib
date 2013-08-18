d5d7ece0f90e28c42799d4e81173276e6b1fcba4
Michael Ernst <mernst@cs.washington.edu>
2013-08-18 13:40:54 -0700

    Backport changes from DFF branch

---
+++ java/src/plume/Filter.java
@@ -0,0 +1,14 @@
+package plume;
+
+/**
+ * Interface for things that make boolean decisions.
+ * This is inspired by java.io.FilenameFilter.
+ **/
+public interface Filter<T> {
+  /**
+   * Tests whether a specified Object satisfies the filter.
+   * @param o the object to test
+   * @return whether the object satisfies the filter
+   */
+  boolean accept(T o);
+}
