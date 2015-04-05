2e0e1f07dfa1490102d4d208736366319f4a7f49
Michael Ernst <mernst@cs.washington.edu>
2015-04-05 16:03:25 -0300

    Update canonical URLs

---
+++ java/src/plume/Filter.java
@@ -0,0 +1,18 @@
+package plume;
+
+/*>>>
+import org.checkerframework.checker.nullness.qual.*;
+*/
+
+/**
+ * Interface for things that make boolean decisions.
+ * This is inspired by java.io.FilenameFilter.
+ **/
+public interface Filter<T extends /*@Nullable*/ Object> {
+  /**
+   * Tests whether a specified Object satisfies the filter.
+   * @param o the object to test
+   * @return whether the object satisfies the filter
+   */
+  boolean accept(T o);
+}
