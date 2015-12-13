7704e4123b974dc57272bedd060745028bc4f50f
Michael Ernst <mernst@cs.washington.edu>
2015-12-13 14:31:14 -0800

    Improve documentation, and fail if Travis request fails

--- bin/trigger-travis.sh
+++ bin/trigger-travis.sh
@@ -1,28 +1,28 @@
 #!/bin/sh -f
 
-# Trigger a new Travis-CI job
+# Trigger a new Travis-CI job.
+# Useful for triggering a dependent build in Travis.
 
-# To use this, do two things:
+# To use this script, do two things:
 # 
 # 1. Set an environment variable TRAVISTOKEN by navigating to
 #   https://travis-ci.org/MYGITHUBID/MYGITHUBPROJECT/settings
 # The environment variable will be set when Travis runs the job,
 # but won't be visible to anyone even though the repository is public.
-# Determine the value to set TRAVISTOKEN to via:  travis login && travis token
-# (You may need to first:  sudo apt-get install ruby-dev && sudo gem install travis
-# Don't do "sudo apt-get install travis"; use the above instead.)
+# Determine the value for TRAVISTOKEN via:  travis login && travis token
+# (You may need to first do:
+#    sudo apt-get install ruby-dev && sudo gem install travis
+# but don't do "sudo apt-get install travis"; use the above instead.)
 # This differs from the token available at https://travis-ci.org/profile .
 # 
-# 1. Add one of the following after-success blocks to your .travis.yml file,
-# where $TRAVISTOKEN is literal text but MYGITHUB* should be replaced:
-#
-# after-success:
-#   - trigger-travis.sh MYGITHUBID MYGITHUBPROJECT $TRAVISTOKEN
+# 1. Add the following after-success block to your .travis.yml file,
+# where you replace MYGITHUB* by a specific downstream project
+# but you leave $TRAVISTOKEN as literal text:
 #
 # after-success:
 #   - curl -s https://raw.githubusercontent.com/mernst/plume-lib/master/bin/trigger-travis.sh > trigger-travis.sh
 #   - sh trigger-travis.sh MYGITHUBID MYGITHUBPROJECT $TRAVISTOKEN
-#
+
 # An alternative to this script would be to install the Travis command-line
 # client and then run,
 #   travis restart -r MYGITHUBID/MYGITHUBPROJECT
@@ -48,4 +48,9 @@ curl -s -X POST \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token ${TOKEN}" \
   -d "$body" \
-  https://api.travis-ci.org/repo/${USER}%2F${REPO}/requests
+  https://api.travis-ci.org/repo/${USER}%2F${REPO}/requests \
+ | tee /tmp/travis-request-output.$$.txt
+
+if grep -q '"@type": "error"' /tmp/travis-request-output.$$.txt; then
+    exit 1
+fi
