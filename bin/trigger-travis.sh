4b69b8f5e5366194cd760f1a4bc859ebdde0def3
Michael Ernst <mernst@cs.washington.edu>
2015-12-30 19:48:31 -0800

    Update documentation of how to write an after_success: block
    Downstream projects are triggered only for builds of the mainline, not
    branches or pull requests, and the test works for matrix or non-matrix builds.

--- bin/trigger-travis.sh
+++ bin/trigger-travis.sh
@@ -8,48 +8,55 @@
 # 
 # 1. Set an environment variable TRAVIS_ACCESS_TOKEN by navigating to
 #   https://travis-ci.org/MYGITHUBID/MYGITHUBPROJECT/settings
-# The environment variable will be set when Travis runs the job,
-# but won't be visible to anyone browsing https://travis-ci.org/.
 # Determine the value for TRAVIS_ACCESS_TOKEN via: travis login && travis token
 # (You may need to first do:
 #    sudo apt-get install ruby-dev && sudo gem install travis
 # Don't do "sudo apt-get install travis" which installs a trajectory analyzer.)
 # This differs from the token available at https://travis-ci.org/profile .
+# The environment variable will be set when Travis runs the job,
+# but won't be visible to anyone browsing https://travis-ci.org/.
 # 
 # 2. Add the following after_success block to your .travis.yml file,
 # where you replace OTHERGITHUB* by a specific downstream project,
-# but you leave $TRAVIS_ACCESS_TOKEN and $TRAVIS_REPO_SLUG as literal text:
+# but you leave $TRAVIS_ACCESS_TOKEN as literal text:
 #
 # after_success:
-#   - curl -LO https://raw.github.com/mernst/plume-lib/master/bin/trigger-travis.sh
-#   - sh trigger-travis.sh OTHERGITHUBID OTHERGITHUBPROJECT $TRAVIS_ACCESS_TOKEN
-
-# There are two caveats to calling this in the "after_success:" block.
-#
-# 1. Travis does not fail a job if an after_success command fails.  If you
-# misspell a GitHub ID or project name, then this script will fail, but
-# Travis won't inform you of the mistake.  So, check the end of the Travis
-# buid log the first time that a build succeeds.
-#
-# 2. This second point is relevant only if your .travis.yml defines a build
-# matrix (https://docs.travis-ci.com/user/customizing-the-build/#Build-Matrix)
-# that runs the same job using different configurations.
-# The "after_success:" block is run once for every build in the matrix, but
-# you only want it to run once if all the builds in the matrix succeed.  For
-# a workaround, see https://github.com/dmakhno/travis_after_all , but I
-# couldn't get its permissions to work and don't know why.  A hack is to have
-# only the first job in the matrix trigger downstream builds.  This isn't
-# perfect, because the downstream job is triggered even if some job other
-# than the first one fails.  However, it is simple and it works.  Write in
-# your .travis.yml file:
-#
-# after_success:
-#   # The vertical bar (pipe) means multi-line command in YAML.
 #   - |
-#       if [[ "$TRAVIS_JOB_NUMBER" == *.1 ]] ; then
+#       if [[ ($TRAVIS_BRANCH == master) &&
+#             ($TRAVIS_PULL_REQUEST == false) &&
+#             ( (! $TRAVIS_JOB_NUMBER == *.*) || ($TRAVIS_JOB_NUMBER == *.1) ) ]] ; then
 #         curl -LO https://raw.github.com/mernst/plume-lib/master/bin/trigger-travis.sh
 #         sh trigger-travis.sh OTHERGITHUBID OTHERGITHUBPROJECT $TRAVIS_ACCESS_TOKEN
 #       fi
+# 
+# Note that Travis does not fail a job if an after_success command fails.
+# If you misspell a GitHub ID or project name, then this script will fail,
+# but Travis won't inform you of the mistake.  So, check the end of the
+# Travis buid log the first time that a build succeeds.
+
+# Here is an explanation of the conditional in the after_success block:
+#
+# 1. Downstream projects are triggered only for builds of the mainline, not
+# branches or pull requests.  The reason is that typically a downstream
+# project clones and uses the mainline.  You could enhance this script to
+# accept pass an environment variable for the upstream project; the
+# downstream project's build script would need to read and use that
+# environment variable.  If you make this enhancement, feel free to submit
+# a pull request so that others can benefit from it.
+#
+# 2. Downstream projects are triggered only if the Travis job number
+# contains no "." or ends with ".1".  In other words, if your .travis.yml
+# defines a build matrix
+# (https://docs.travis-ci.com/user/customizing-the-build/#Build-Matrix)
+# that runs the same job using different configurations, then the
+# "after_success:" block is run only for the first configuration.  By
+# default an after_success: block is run for every build in the matrix, but
+# you really want it to run once if all the builds in the matrix succeed.
+# For a workaround, see https://github.com/dmakhno/travis_after_all , but I
+# couldn't get its permissions to work and don't know why.  The given test
+# is a hack, because the downstream job is triggered even if some job other
+# than the first one fails.  However, the given test is simple and it is
+# usually adequate.
 
 # An alternative to this script would be to install the Travis command-line
 # client and then run:
@@ -70,7 +77,8 @@ elif [ -n "$TRAVIS_REPO_SLUG" ] ; then
 else
     MESSAGE=""
 fi
-echo "MESSAGE=$MESSAGE"
+## For debugging:
+# echo "MESSAGE=$MESSAGE"
 
 body="{
 \"request\": {
