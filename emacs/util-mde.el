f61b80a196c78a39536253d798f8e4582d294343
Michael Ernst <mernst@cs.washington.edu>
2016-01-20 05:33:05 -0800

    Add shuffle to randomize a list

--- emacs/util-mde.el
+++ emacs/util-mde.el
@@ -360,6 +360,21 @@ after that, its first partial elements appear.  The length of LIST is
   (nth (% (abs (random)) (length list)) list))
 
 
+(defun swap (LIST el1 el2)
+  "In LIST, swap elements at indices EL1 and EL2 in place."
+  (let ((tmp (elt LIST el1)))
+    (setf (elt LIST el1) (elt LIST el2))
+    (setf (elt LIST el2) tmp)))
+
+(defun shuffle (LIST)
+  "Shuffle the elements in LIST.
+shuffling is done in place."
+  (loop for i in (reverse (number-sequence 1 (1- (length LIST))))
+        do (let ((j (random (+ i 1))))
+             (swap LIST i j)))
+  LIST)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Functions
 ;;;
