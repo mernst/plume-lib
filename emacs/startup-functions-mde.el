c81ae36cc0bf3addbf9bbd81ba187190dd482cc2
Michael Ernst <mernst@cs.washington.edu>
2016-03-31 10:01:14 -0700

    Offer to make buffer modifiable

--- emacs/startup-functions-mde.el
+++ emacs/startup-functions-mde.el
@@ -904,6 +904,27 @@ Not guaranteed to work in all cases."
   (set-input-method "spanish-postfix"))
 
 
+(defun offer-to-change-if-read-only ()
+  (if buffer-read-only
+      (progn
+	(if (y-or-n-p "Buffer is read-only.  Make buffer modifiable? ")
+	    (setq buffer-read-only nil))))
+  (barf-if-buffer-read-only))
+
+
+(defadvice flush-lines (before make-buffer-modifiable activate)
+  (interactive
+   (progn
+     (offer-to-change-if-read-only)
+     (keep-lines-read-args "Flush lines containing match for regexp"))))
+
+(defadvice keep-lines (before make-buffer-modifiable activate)
+  (interactive
+   (progn
+     (offer-to-change-if-read-only)
+     (keep-lines-read-args "Keep lines containing match for regexp"))))
+
+
 ;; The defaliases and defun are copied verbatim from flush-lines, then
 ;; replace each instance of "(forward-line 0)" by "(backward-paragraph)"
 ;; and then "line" by "paragraph".  Not well tested.  One known bug:
@@ -950,7 +971,7 @@ a previously found match."
 
   (interactive
    (progn
-     (barf-if-buffer-read-only)
+     (offer-to-change-if-read-only)
      (keep-lines-read-args "Keep paragraphs (containing match for regexp)")))
   (if rstart
       (progn
@@ -1022,7 +1043,7 @@ starting on the same paragraph at which another match ended is ignored."
 
   (interactive
    (progn
-     (barf-if-buffer-read-only)
+     (offer-to-change-if-read-only)
      (keep-lines-read-args "Flush paragraphs containing match for regexp")))
   (if rstart
       (progn
