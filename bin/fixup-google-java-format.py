c0ef52b44a3ad59291a5d97c4f61a76127737e4e
Michael Ernst <mernst@cs.washington.edu>
2016-04-29 04:13:09 -0700

    Improve formatting for commented receivers (remove spurious space)

--- bin/fixup-google-java-format.py
+++ bin/fixup-google-java-format.py
@@ -105,6 +105,9 @@ debug = False
 # Two annotations in a row, or an annotation abutting array brackets "[]".
 # Space is inserted between.
 abuttingannoRegex = re.compile(r"(/\*@[A-Za-z0-9_]+\*/)(\[\]|/\*@[A-Za-z0-9_]+\*/)")
+# Voodoo annotation with extra space after
+vodootrailingspaceRegex = re.compile(r"(\(/\*>>> @.*\bthis\*/) (\))")
+
 # An annotation at the end of its line.
 # The annotation will be moved to the beginning of the following line.
 trailingannoRegex = re.compile(r"^(.*?)[ \t]*(/\*@[A-Za-z0-9_]+\*/)$")
@@ -121,6 +124,7 @@ def fixup_loop(infile, outfile):
     """Fix up formatting while reading from infile and writing to outfile."""
     prev = ""           # previous line
     for line in infile:
+        line = vodootrailingspaceRegex.sub(r"\1\2", line)
         m = re.search(abuttingannoRegex, line)
         while m:
             if debug: print "found abutting", line
