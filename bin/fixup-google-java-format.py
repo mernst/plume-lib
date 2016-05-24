b0bd40cf5438678527777a8dafa3ec22457f2291
Michael Ernst <mernst@cs.washington.edu>
2016-05-24 10:49:06 -0700

    Handle annotations with arguments

--- bin/fixup-google-java-format.py
+++ bin/fixup-google-java-format.py
@@ -6,98 +6,103 @@
 # small changes in place to improve formatting of annotations in comments.
 # If called with no arguments, it reads from and writes to standard output.
 
+from __future__ import print_function
+
 import os
 import re
 import sys
 
-# TODO: handle annotations with arguments
+def eprint(*args, **kwargs):
+    "Print to standard error"
+    print(*args, file=sys.stderr, **kwargs)
+
 
 # These are annotations defined in the Checker Framework that *should* go
 # on their own line.
 # (To generate this list, search for occurrences of "@Target("
 # and remove those that contain "TYPE_USE" or "{}".)
 declarationAnnotations = set([
-    "/*@GetConstructor*/",
-    "/*@NewInstance*/",
-    "/*@Invoke*/",
-    "/*@GetClass*/",
-    "/*@GetMethod*/",
-    "/*@ForName*/",
-    "/*@ReportCreation*/",
-    "/*@ReportReadWrite*/",
-    "/*@ReportWrite*/",
-    "/*@ReportOverride*/",
-    "/*@ReportInherit*/",
-    "/*@ReportCall*/",
-    "/*@ReportUse*/",
-    "/*@StaticallyExecutable*/",
-    "/*@InheritedAnnotation*/",
-    "/*@FromByteCode*/",
-    "/*@PostconditionAnnotation*/",
-    "/*@RequiresQualifiers*/",
-    "/*@DefaultQualifier*/",
-    "/*@DefaultQualifierInHierarchyInUncheckedCode*/",
-    "/*@InvisibleQualifier*/",
-    "/*@TargetLocations*/",
-    "/*@RequiresQualifier*/",
-    "/*@ImplicitFor*/",
-    "/*@StubFiles*/",
-    "/*@EnsuresQualifiersIf*/",
-    "/*@EnsuresQualifier*/",
-    "/*@DefaultInUncheckedCodeFor*/",
-    "/*@AnnotatedFor*/",
-    "/*@PreconditionAnnotation*/",
-    "/*@ConditionalPostconditionAnnotation*/",
-    "/*@DefaultQualifiers*/",
-    "/*@FieldIsExpression*/",
-    "/*@FromStubFile*/",
-    "/*@Unused*/",
-    "/*@EnsuresQualifiers*/",
-    "/*@PolymorphicQualifier*/",
-    "/*@SubtypeOf*/",
-    "/*@DefaultQualifierInHierarchy*/",
-    "/*@DefaultFor*/",
-    "/*@EnsuresQualifierIf*/",
-    "/*@MonotonicQualifier*/",
-    "/*@Pure*/",
-    "/*@LockingFree*/",
-    "/*@TerminatesExecution*/",
-    "/*@Deterministic*/",
-    "/*@SideEffectFree*/",
-    "/*@SafeType*/",
-    "/*@SafeEffect*/",
-    "/*@UIType*/",
-    "/*@UIPackage*/",
-    "/*@PolyUIEffect*/",
-    "/*@PolyUIType*/",
-    "/*@UIEffect*/",
-    "/*@NotOnlyInitialized*/",
-    "/*@ClassRegexParam*/",
-    "/*@MethodRegexParam*/",
-    "/*@MultiMethodRegexParam*/",
-    "/*@Assignable*/",
-    "/*@I18nValidFormat*/",
-    "/*@I18nMakeFormat*/",
-    "/*@Assignable*/",
-    "/*@Assignable*/",
-    "/*@EnsuresLockHeldIf*/",
-    "/*@HoldingOnEntry*/",
-    "/*@EnsuresLockHeld*/",
-    "/*@Holding*/",
-    "/*@FormatMethod*/",
-    "/*@ReturnsFormat*/",
-    "/*@EnsuresNonNull*/",
-    "/*@Covariant*/",
-    "/*@EnsuresNonNullIf*/",
-    "/*@RequiresNonNull*/",
-    "/*@AssertNonNullIfNonNull*/",
-    "/*@UsesObjectEquals*/",
-    "/*@I18nChecksFormat*/",
-    "/*@MultiClassRegexParam*/",
-    "/*@MethodTaintingParam*/",
-    "/*@MultiMethodTaintingParam*/",
-    "/*@ClassTaintingParam*/",
-    "/*@MultiClassTaintingParam*/",
+    "@GetConstructor",
+    "@NewInstance",
+    "@Invoke",
+    "@GetClass",
+    "@GetMethod",
+    "@ForName",
+    "@ReportCreation",
+    "@ReportReadWrite",
+    "@ReportWrite",
+    "@ReportOverride",
+    "@ReportInherit",
+    "@ReportCall",
+    "@ReportUse",
+    "@StaticallyExecutable",
+    "@InheritedAnnotation",
+    "@FromByteCode",
+    "@PostconditionAnnotation",
+    "@RequiresQualifiers",
+    "@DefaultQualifier",
+    "@DefaultQualifierInHierarchyInUncheckedCode",
+    "@InvisibleQualifier",
+    "@TargetLocations",
+    "@RequiresQualifier",
+    "@ImplicitFor",
+    "@StubFiles",
+    "@EnsuresQualifiersIf",
+    "@EnsuresQualifier",
+    "@DefaultInUncheckedCodeFor",
+    "@AnnotatedFor",
+    "@PreconditionAnnotation",
+    "@ConditionalPostconditionAnnotation",
+    "@DefaultQualifiers",
+    "@FieldIsExpression",
+    "@FromStubFile",
+    "@Unused",
+    "@EnsuresQualifiers",
+    "@PolymorphicQualifier",
+    "@SubtypeOf",
+    "@DefaultQualifierInHierarchy",
+    "@DefaultFor",
+    "@EnsuresQualifierIf",
+    "@MonotonicQualifier",
+    "@Pure",
+    "@LockingFree",
+    "@TerminatesExecution",
+    "@Deterministic",
+    "@SideEffectFree",
+    "@SafeType",
+    "@SafeEffect",
+    "@UIType",
+    "@UIPackage",
+    "@PolyUIEffect",
+    "@PolyUIType",
+    "@UIEffect",
+    "@NotOnlyInitialized",
+    "@ClassRegexParam",
+    "@MethodRegexParam",
+    "@MultiMethodRegexParam",
+    "@Assignable",
+    "@I18nValidFormat",
+    "@I18nMakeFormat",
+    "@Assignable",
+    "@Assignable",
+    "@EnsuresLockHeldIf",
+    "@HoldingOnEntry",
+    "@EnsuresLockHeld",
+    "@Holding",
+    "@FormatMethod",
+    "@ReturnsFormat",
+    "@EnsuresNonNull",
+    "@Covariant",
+    "@EnsuresNonNullIf",
+    "@RequiresNonNull",
+    "@AssertNonNullIfNonNull",
+    "@UsesObjectEquals",
+    "@I18nChecksFormat",
+    "@MultiClassRegexParam",
+    "@MethodTaintingParam",
+    "@MultiMethodTaintingParam",
+    "@ClassTaintingParam",
+    "@MultiClassTaintingParam",
 ])
 
 debug = False
@@ -112,7 +117,7 @@ voodootrailingspaceRegex = re.compile(r"(/\*>>> ?@.*\bthis\*/) (\))")
 # Also matches an annotation like /*offset = */ that should appear right
 # before the argument it documents.
 # The annotation will be moved to the beginning of the following line.
-trailingannoRegex = re.compile(r"^(.*?)[ \t]*(/\*(@[A-Za-z0-9_]+| *[A-Za-z0-9_]+ *= *)\*/)$")
+trailingannoRegex = re.compile(r"^(.*?)[ \t]*(/\*(@[A-Za-z0-9_().\"]+| *[A-Za-z0-9_]+ *= *)\*/)$")
 
 whitespaceRegex = re.compile(r"^([ \t]*).*$")
 
@@ -133,7 +138,7 @@ def fixup_loop(infile, outfile):
         # Handle abutting annotations in comments
         m = re.search(abuttingannoRegex, line)
         while m:
-            if debug: print "found abutting", line
+            if debug: print("found abutting", line)
             line = line[0:m.end(1)] + " " + line[m.start(2):]
             m = re.search(abuttingannoRegex, line)
         # Handle annotations at end of line that should be at beginning of
@@ -141,21 +146,36 @@ def fixup_loop(infile, outfile):
         m = re.search(trailingannoRegex, prev)
         while m:
             anno = m.group(2)
-            if anno in declarationAnnotations:
+            if base_annotation(anno) in declarationAnnotations:
                 break
-            if debug: print "prev was:", prev
+            if debug: print("prev was:", prev)
             prev = prev[0:m.end(1)] + prev[m.end(2):]
-            if debug: print "prev is :", prev
+            if debug: print("prev is :", prev)
             if re.search(emptylineRegex, prev):
                 prev = ""
-                if debug: print "prev is':", prev
-            if debug: print "line was:", line
+                if debug: print("prev is':", prev)
+            if debug: print("line was:", line)
             line = insert_after_whitespace(m.group(2) + " ", line)
-            if debug: print "line is :", line
+            if debug: print("line is :", line)
             m = re.search(trailingannoRegex, prev)
         outfile.write(prev)
         prev = line
     outfile.write(prev)
+
+def base_annotation(annotation):
+    """Remove leading and trailing comment characters, spaces, and arguments.
+Example: base_annotation('/*@RequiresNonNull("FileIO.data_trace_state")*/' => '@RequiresNonNull'"""
+    if debug: print("base_annotation <=", annotation)
+    if annotation.startswith("/*"):
+        annotation = annotation[2:]
+    if annotation.endswith("*/"):
+        annotation = annotation[:-2]
+    idx = annotation.find('(')
+    if idx != -1:
+        annotation = annotation[0:idx]
+    annotation = annotation.strip()
+    if debug: print("base_annotation =>", annotation)
+    return annotation
 
 
 if len(sys.argv) == 1:
