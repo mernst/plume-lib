9fda0f81841655f97fd8513fa09c7b4102e93e32
Michael Ernst <mernst@cs.washington.edu>
2016-06-23 22:46:06 -0700

    Only move known type annotations

--- bin/fixup-google-java-format.py
+++ bin/fixup-google-java-format.py
@@ -19,102 +19,181 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-# These are annotations defined in the Checker Framework that should go on
-# their own line.  Their @Target annotation contains neither "TYPE_USE" nor
-# "{}".  To generate this list (last done on 2016-06-23):
-#   ag --file-search-regex src/org/checkerframework --files-with-matches '^@Target\b(?!.*TYPE_USE)(?!.{})' $cf | sed 's/.*\///' | sed 's/\(.*\)\.java/    "\1",/' | sort
-declarationAnnotationsBuiltin = set([
-    "AnnotatedFor",
-    "AssertNonNullIfNonNull",
-    "ClassRegexParam",
-    "ClassTaintingParam",
-    "ConditionalPostconditionAnnotation",
-    "Covariant",
-    "DefaultFor",
-    "DefaultInUncheckedCodeFor",
-    "DefaultQualifier",
-    "DefaultQualifierInHierarchy",
-    "DefaultQualifierInHierarchyInUncheckedCode",
-    "DefaultQualifiers",
-    "Deterministic",
-    "EnsuresLockHeld",
-    "EnsuresLockHeldIf",
-    "EnsuresNonNull",
-    "EnsuresNonNullIf",
-    "EnsuresQualifier",
-    "EnsuresQualifierIf",
-    "EnsuresQualifiers",
-    "EnsuresQualifiersIf",
-    "FieldIsExpression",
-    "FormatMethod",
-    "ForName",
-    "FromByteCode",
-    "FromStubFile",
-    "GetClass",
-    "GetConstructor",
-    "GetMethod",
-    "Holding",
-    "I18nChecksFormat",
-    "I18nMakeFormat",
-    "I18nValidFormat",
-    "IgnoreInWholeProgramInference",
-    "ImplicitFor",
-    "InheritedAnnotation",
-    "InvisibleQualifier",
-    "Invoke",
-    "LockingFree",
-    "MayReleaseLocks",
-    "MethodRegexParam",
-    "MethodTaintingParam",
-    "MonotonicQualifier",
-    "MultiClassRegexParam",
-    "MultiClassTaintingParam",
-    "MultiMethodRegexParam",
-    "MultiMethodTaintingParam",
-    "NewInstance",
-    "NotOnlyInitialized",
-    "PolymorphicQualifier",
-    "PolyUIEffect",
-    "PolyUIType",
-    "PostconditionAnnotation",
-    "PreconditionAnnotation",
-    "Pure",
-    "ReleasesNoLocks",
-    "ReportCall",
-    "ReportCreation",
-    "ReportInherit",
-    "ReportOverride",
-    "ReportReadWrite",
-    "ReportUse",
-    "ReportWrite",
-    "RequiresNonNull",
-    "RequiresQualifier",
-    "RequiresQualifiers",
-    "ReturnsFormat",
-    "SafeEffect",
-    "SafeType",
-    "SideEffectFree",
-    "StaticallyExecutable",
-    "StubFiles",
-    "SubtypeOf",
-    "SupportedLintOptions",
-    "SupportedOptions",
-    "SuppressWarningsKeys",
-    "TargetLocations",
-    "TerminatesExecution",
-    "UIEffect",
-    "UIPackage",
-    "UIType",
-    "Unused",
-    "UsesObjectEquals",
+# These are annotations that should not go on their own line.
+# They are type annotations: their @Target annotation contains "TYPE_USE".
+# To generate this list (last done on 2016-06-23):
+#   ag --file-search-regex src/org/checkerframework --files-with-matches '^@Target\b.*TYPE_USE' $cf | sed 's/.*\///' | sed 's/\(.*\)\.java/    "\1",/' | sort | uniq
+typeAnnotations = set([
+    "A",
+    "Acceleration",
+    "AlwaysSafe",
+    "Angle",
+    "Area",
+    "ArrayLen",
+    "AwtAlphaCompositingRule",
+    "AwtColorSpace",
+    "AwtCursorType",
+    "AwtFlowLayout",
+    "BinaryName",
+    "BinaryNameForNonArray",
+    "BinaryNameForNonArrayInUnnamedPackage",
+    "BinaryNameInUnnamedPackage",
+    "BoolVal",
+    "Bottom",
+    "BottomVal",
+    "C",
+    "cd",
+    "ClassBound",
+    "ClassGetName",
+    "ClassGetSimpleName",
+    "ClassVal",
+    "ClassValBottom",
+    "CompilerMessageKey",
+    "Constant",
+    "Current",
+    "degrees",
+    "Dependent",
+    "DoubleVal",
+    "FBCBottom",
+    "Fenum",
+    "FenumBottom",
+    "FenumTop",
+    "FieldDescriptor",
+    "FieldDescriptorForArray",
+    "Format",
+    "FormatBottom",
+    "FullyQualifiedName",
+    "g",
+    "GuardedBy",
+    "GuardedByBottom",
+    "GuardedByUnknown",
+    "GuardSatisfied",
+    "h",
+    "I18nFormat",
+    "I18nFormatBottom",
+    "I18nFormatFor",
+    "I18nInvalidFormat",
+    "I18nUnknownFormat",
+    "Identifier",
+    "IdentifierOrArray",
+    "Initialized",
+    "InternalForm",
+    "Interned",
+    "IntVal",
+    "InvalidFormat",
+    "K",
+    "KeyFor",
+    "KeyForBottom",
+    "KeyForType",
+    "kg",
+    "km",
+    "km2",
+    "kmPERh",
+    "LazyNonNull",
+    "LeakedToResult",
+    "Length",
+    "Linear",
+    "LocalizableKey",
+    "Localized",
+    "Luminance",
+    "m",
+    "m2",
+    "Mass",
+    "MaybeAliased",
+    "MethodDescriptor",
+    "MethodVal",
+    "MethodValBottom",
+    "min",
+    "mm",
+    "mm2",
+    "mol",
+    "MonotonicNonNull",
+    "MonotonicNonNullType",
+    "mPERs",
+    "mPERs2",
+    "MultiPolyRegex",
+    "MultiPolyTainted",
+    "MultiRegex",
+    "MultiTainted",
+    "MultiUntainted",
+    "MultiVar",
+    "MultiWild",
+    "NonLeaked",
+    "NonNull",
+    "NonNullType",
+    "NonRaw",
+    "Normal",
+    "Nullable",
+    "NullableType",
+    "PolyAll",
+    "PolyGuardedBy",
+    "PolyInterned",
+    "PolyKeyFor",
+    "PolyNull",
+    "PolyNullType",
+    "PolyRaw",
+    "PolyRegex",
+    "PolySignature",
+    "PolyTainted",
+    "PolyUI",
+    "PolyUnit",
+    "PropertyKey",
+    "PropertyKeyBottom",
+    "PurityUnqualified",
+    "radians",
+    "Raw",
+    "Regex",
+    "RegexBottom",
+    "ReportUnqualified",
+    "s",
+    "SignatureBottom",
+    "Signed",
+    "SourceNameForNonArrayNonInner",
+    "SourceNameForNonInner",
+    "Speed",
+    "StringVal",
+    "Substance",
+    "SwingBoxOrientation",
+    "SwingCompassDirection",
+    "SwingElementOrientation",
+    "SwingHorizontalOrientation",
+    "SwingSplitPaneOrientation",
+    "SwingTextOrientation",
+    "SwingTitleJustification",
+    "SwingTitlePosition",
+    "SwingVerticalOrientation",
+    "Tainted",
+    "Temperature",
+    "Time",
+    "UI",
+    "UnderInitialization",
+    "Unique",
+    "UnitsBottom",
+    "UnknownClass",
+    "UnknownInitialization",
+    "UnknownInterned",
+    "UnknownKeyFor",
+    "UnknownLocalized",
+    "UnknownMethod",
+    "UnknownPropertyKey",
+    "UnknownRegex",
+    "UnknownSignedness",
+    "UnknownUnits",
+    "UnknownVal",
+    "Unsigned",
+    "UnsignednessBottom",
+    "Untainted",
+    "Unusable",
+    "Var",
+    "Wild",
 ])
 
-if os.path.isfile(".declaration-annotations"):
+# File .type-annotations can add to the typeAnnotations variable.
+if os.path.isfile(".type-annotations"):
     execfile('execfile_example.py')
-else:
-    declarationAnnotations = declarationAnnotationsBuiltin
 
 debug = False
+# debug = True
 
 # Two annotations in a row, or an annotation abutting array brackets "[]".
 # Space is inserted between.
@@ -122,18 +201,23 @@ abuttingannoRegex = re.compile(r"(/\*@[A-Za-z0-9_]+\*/)(\[\]|/\*@[A-Za-z0-9_]+\*
 # Voodoo annotation with extra space after
 voodootrailingspaceRegex = re.compile(r"(/\*>>> ?@.*\bthis\*/) (\))")
 
-# An annotation at the end of its line (in capturing group 2).
-# Also matches an annotation like /*offset = */ that should appear right
-# before the argument it documents.
-# The annotation will be moved to the beginning of the following line.
-# TODO: should also match non-commented annotations.
-# TODO: should search in classpath to determine whether annotation is a type
-# annotation, and only move it to the beginning of the following line if so.
-trailingannoRegex = re.compile(r"^(.*?)[ \t]*(/\*(@[A-Za-z0-9_().\"]+| *[A-Za-z0-9_]+ *= *)\*/)$")
+# Matches, at the end of its line (in capturing group 2):
+#  * An annotation
+#    This is a bit dangerous!  It might be within a comment.
+#    The script tries to heuristically detect this.
+#  * An annotation in comments
+#  * a comment like /*offset = */ that should appear right
+#    before the argument it documents.
+# The annotation will be moved to the beginning of the following line,
+# if it appears in typeAnnotations.
+trailingannoRegex = re.compile(r"^(.*?)[ \t]*(@[A-Za-z0-9_().\"]+|/\*@[A-Za-z0-9_().\"]+\*/|/\* *[A-Za-z0-9_]+ *= *\*/)$")
 
 whitespaceRegex = re.compile(r"^([ \t]*).*$")
 
 emptylineRegex = re.compile(r"^[ \t]*$")
+
+# Heuristic: matches if the line might be within a //, /*, or Javadoc comment.
+withinCommentRegex = re.compile(r"//|/\*(?!.*\/*/)|^[ \t]*\*[ \t]")
 
 def insert_after_whitespace(insertion, s):
     """Return s, with insertion inserted after its leading whitespace."""
@@ -156,22 +240,28 @@ def fixup_loop(infile, outfile):
         # Handle annotations at end of line that should be at beginning of
         # next line.
         m = re.search(trailingannoRegex, prev)
-        if debug: print("trailing?", m, prev, line)
+        if debug: print("trailing? (pre-loop)", m, prev, line)
         while m:
             if debug: print("found trailing", prev, line)
             anno = m.group(2)
-            if base_annotation(anno) in declarationAnnotations:
+            if not base_annotation(anno) in typeAnnotations:
                 break
             if debug: print("prev was:", prev)
-            prev = prev[0:m.end(1)] + prev[m.end(2):]
-            if debug: print("prev is :", prev)
+            candidate_prev = prev[0:m.end(1)] + prev[m.end(2):]
+            if debug: print("candidate_prev is :", candidate_prev)
+            if re.search(withinCommentRegex, candidate_prev):
+                if debug: print("withinCommentRegex prohibits action")
+                break
+            prev = candidate_prev
+            if debug: print("prev is:", prev)
             if re.search(emptylineRegex, prev):
                 prev = ""
-                if debug: print("prev is':", prev)
+                if debug: print("prev is empty")
             if debug: print("line was:", line)
-            line = insert_after_whitespace(m.group(2) + " ", line)
+            line = insert_after_whitespace(anno + " ", line)
             if debug: print("line is :", line)
             m = re.search(trailingannoRegex, prev)
+            if debug: print("trailing? (post-loop-body)", m, prev, line)
         outfile.write(prev)
         prev = line
     outfile.write(prev)
