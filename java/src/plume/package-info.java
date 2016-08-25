209138d9b351e4468ac1e34c2a9af6c55c9f0021
Michael Ernst <mernst@cs.washington.edu>
2016-08-25 05:33:10 -0700

    Update URLs

--- java/src/plume/package-info.java
+++ java/src/plume/package-info.java
@@ -1,10 +1,10 @@
 /**
  * <p>
  * The <code>plume</code> package contains the Java component of <a
- * href="http://mernst.github.io/plume-lib/">plume-lib</a>,
+ * href="https://mernst.github.io/plume-lib/">plume-lib</a>,
  * which contains a variety of useful programs and libraries; for more
  * details, please see the
- * <a href="http://mernst.github.io/plume-lib/">plume-lib
+ * <a href="https://mernst.github.io/plume-lib/">plume-lib
  * documentation</a>.
  * </p>
  */
