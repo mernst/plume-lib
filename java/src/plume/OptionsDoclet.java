7626f9f3cf68afc43acb590520a087f03c724de2
Michael Ernst <mernst@cs.washington.edu>
2017-06-30 12:42:52 -0700

    Update libraries

--- java/src/plume/OptionsDoclet.java
+++ java/src/plume/OptionsDoclet.java
@@ -31,8 +31,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 /*>>>
 import org.checkerframework.checker.formatter.qual.*;
