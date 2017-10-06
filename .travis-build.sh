86ebbbdaca8b41d99cf72ae312017b88592ef1f0
Michael Ernst <mernst@cs.washington.edu>
2017-10-06 12:43:18 -0700

    Run pluggable type-checking in Travis

---
+++ .travis-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ROOT=$TRAVIS_BUILD_DIR/..
+
+# Fail the whole script if any command fails
+set -e
+
+## Build Checker Framework
+(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# This also builds annotation-tools and jsr308-langtools
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
+export CHECKERFRAMEWORK=$ROOT/checker-framework
+
+## Obtain plume-lib
+(cd $ROOT && git clone https://github.com/mernst/plume-lib.git)
+
+make -C $ROOT/plume-lib/java check-types
