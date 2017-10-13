de6759c54112e1e19354a4a5e3e21487b9ef5acb
Michael Ernst <mernst@cs.washington.edu>
2017-10-13 11:32:26 -0700

    Temporarily use a branch of the Checker Framework

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,7 +5,9 @@ ROOT=$TRAVIS_BUILD_DIR/..
 set -e
 
 ## Build Checker Framework
-(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+## Temporary: use a fork of the Checker Framework
+(cd $ROOT && git clone https://github.com/panacekcz/checker-framework.git --branch strings-indexof)
 # This also builds annotation-tools and jsr308-langtools
 (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
