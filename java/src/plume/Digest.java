10884080b29f60e165755fc1110c2be8cd75a1fd
Michael Ernst <mernst@cs.washington.edu>
2017-10-26 22:00:41 -0700

    Improve warning suppression messages

--- java/src/plume/Digest.java
+++ java/src/plume/Digest.java
@@ -67,8 +67,7 @@ public final class Digest {
    * @param bytes the bytes to convert to a String
    * @return a String representation of the input bytes
    */
-  @SuppressWarnings(
-      "upperbound") // the accesses to digits rely on the masks and the shift correctly creating an index
+  @SuppressWarnings("upperbound") // arithmetic (masks and shift) creating an index
   public static String hexEncode(byte[] bytes) {
     StringBuffer s = new StringBuffer(bytes.length * 2);
     for (int i = 0; i < bytes.length; i++) {
