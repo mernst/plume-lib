5559ca0a65de20d448142b99b1acaa6c5e81e090
kelloggm <kelloggm@cs.washington.edu>
2018-01-19 18:47:21 -0800

    modify warning suppression so that both index and value are suppressed

--- java/src/plume/TestPlume.java
+++ java/src/plume/TestPlume.java
@@ -2209,7 +2209,7 @@ public final class TestPlume {
             }
           }
           @SuppressWarnings(
-              "index") // The IotaIterator only contains indexes for totals.length, and since chosen's elements are selected randomly from the IotaIterator, all of its elements are @IndexFor
+                  {"index", "value"}) // The IotaIterator only contains indexes for totals.length, and since chosen's elements are selected randomly from the IotaIterator, all of its elements are @IndexFor
           List</*@IndexFor("totals")*/ Integer> chosen =
               UtilMDE.randomElements(new IotaIterator(itor_size), i, r);
           for (int m = 0; m < chosen.size(); m++) {
