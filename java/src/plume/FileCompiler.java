f8377ce373875ccfeecd1d78ceeae42d7221d91f
Michael Ernst <mernst@cs.washington.edu>
2018-02-25 14:24:54 -0800

    Suppress nullness error

--- java/src/plume/FileCompiler.java
+++ java/src/plume/FileCompiler.java
@@ -167,7 +167,9 @@ public final class FileCompiler {
 
     cmdLine = new CommandLine(compiler[0]); // constructor requires executable name
     // add rest of compiler command arguments
-    cmdLine.addArguments(Arrays.copyOfRange(compiler, 1, compiler.length));
+    @SuppressWarnings("nullness") // arguments are in range, so result array contains no nulls
+    /*@NonNull*/ String[] args = Arrays.copyOfRange(compiler, 1, compiler.length);
+    cmdLine.addArguments(args);
     // add file name arguments
     cmdLine.addArguments(filenames.toArray(new String[0]));
 
